e4b6da29bc8a7adaa44cc49a9f53a45e880534af
Delta <delta6862@protonmail.com>
2021-01-30 11:07:54 +0100

    Updated the statistics
    Signed-off-by: Delta <delta6862@protonmail.com>

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 from Stad import *
 from Network import *
 
-city = game.Game(tick_speed=0, tick_cap=50, stats=True)
+city = game.Game(tick_speed=0, tick_cap=500, stats=True)
 statistic_data = city.get_stats()
 
+# Render statistics
+fig, ax = plt.subplots(ncols=3, figsize=(18, 6))
+ax_index = 0    # Bit ugly, but it works
 for i in statistic_data:
-    fig, ax = plt.subplots()
     x, y = statistic_data[i]
-    ax.title.set_text(i)
-    ax.grid()
-    ax.plot(x, y)
-
+    ax[ax_index].title.set_text(i)
+    ax[ax_index].grid()
+    ax[ax_index].plot(x, y)
+    ax_index += 1
 plt.show()
