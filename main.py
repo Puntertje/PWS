26e7f97d23cb59cec278ca9cd3f88040f81858f4
Delta <delta6862@protonmail.com>
2021-01-30 11:34:13 +0100

    Updated the statistics, now allows for multi-game stats
    Signed-off-by: Delta <delta6862@protonmail.com>

--- main.py
+++ main.py
@@ -2,16 +2,29 @@ import matplotlib.pyplot as plt
 from Stad import *
 from Network import *
 
-city = game.Game(tick_speed=0, tick_cap=500, stats=True)
-statistic_data = city.get_stats()
+fig, ax = plt.subplots(ncols=3, figsize=(18, 6))
+statistic_data = {
+            "Score": [],
+            "Cars": [],
+            "Average score": []
+        }
+
+for _ in range(3):      # number of times you want to run the game
+    city = game.Game(tick_speed=0, tick_cap=50, stats=True)
+
+    # Process statistics
+    statistic_data_temp = city.get_stats()
+    for i in statistic_data_temp:
+        statistic_data[i].append(statistic_data_temp[i])
+
 
 # Render statistics
-fig, ax = plt.subplots(ncols=3, figsize=(18, 6))
-ax_index = 0    # Bit ugly, but it works
+ax_index = 0  # Bit ugly, but it works
 for i in statistic_data:
-    x, y = statistic_data[i]
-    ax[ax_index].title.set_text(i)
-    ax[ax_index].grid()
-    ax[ax_index].plot(x, y)
+    for data in statistic_data[i]:
+        x, y = data
+        ax[ax_index].title.set_text(i)
+        ax[ax_index].grid()
+        ax[ax_index].plot(x, y)
     ax_index += 1
 plt.show()
